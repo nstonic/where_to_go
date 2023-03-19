9709716e50e45e74d34abae8267886a7a1d23d9f
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-19 21:34:30 +0400

    Create places app

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from places.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
