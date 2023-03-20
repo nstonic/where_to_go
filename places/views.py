9c96ef8e7debbec5ddb3cc4a385bf77af4596646
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-20 10:33:44 +0400

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 
+from where_to_go.settings import STATIC_URL
+
 
 def index(request):
-    return render(request, 'index.html')
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676]
+                },
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": f"{STATIC_URL}places/moscow_legends.json"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": f"{STATIC_URL}places/roofs24.json"
+                }
+            }
+        ]
+    }
+    return render(request, 'index.html', context={'places_geojson': places_geojson})
