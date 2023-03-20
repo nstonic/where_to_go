d579c5265c74554a4746b829e92de4ea083b9acd
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-20 23:01:33 +0400

    Rename var > host

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = [var.strip() for var in os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')]
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')]
 
 # Application definition
 
