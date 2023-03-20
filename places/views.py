79bf7aab62c5a213f3a93dac200a4c03232d9b92
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-20 22:51:19 +0400

    Revert "Delete all()"
    This reverts commit bfef7b5a9537cf744501d2491b3f85c08a22dcd1.

--- places/views.py
+++ places/views.py
@@ -7,7 +7,7 @@ from places.models import Place
 
 
 def index(request):
-    places = Place.objects.iterator()
+    places = Place.objects.all().iterator()
     places_geojson = {"type": "FeatureCollection", "features": []}
     for place in places:
         places_geojson["features"].append(
