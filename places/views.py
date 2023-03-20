6b7ac26b7709bf0b0c37230a852054c5abc40f7c
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-20 10:51:48 +0400

    Get points from db

--- places/views.py
+++ places/views.py
@@ -1,36 +1,21 @@
 from django.shortcuts import render
 
+from places.models import Place
 from where_to_go.settings import STATIC_URL
 
 
 def index(request):
-    places_geojson = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": f"{STATIC_URL}places/moscow_legends.json"
-                }
+    places = Place.objects.all().iterator()
+    places_geojson = {"type": "FeatureCollection", "features": []}
+    for place in places:
+        places_geojson["features"].append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.longitude, place.latitude]
             },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": f"{STATIC_URL}places/roofs24.json"
-                }
-            }
-        ]
-    }
-    return render(request, 'index.html', context={'places_geojson': places_geojson})
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": f"{STATIC_URL}places/moscow_legends.json"}})
+    return render(request, "index.html", context={"places_geojson": places_geojson})
