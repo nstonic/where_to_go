a30a2831adebf33f8d26b6950509ad715a481f0a
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-24 11:21:42 +0400

    Fix backets

--- places/views.py
+++ places/views.py
@@ -7,35 +7,35 @@ from places.models import Place
 
 def index(request):
     places = Place.objects.all()
-    places_geojson = {"type": "FeatureCollection", "features": []}
+    places_geojson = {'type': 'FeatureCollection', 'features': []}
     for place in places:
-        places_geojson["features"].append(
+        places_geojson['features'].append(
             {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [place.longitude, place.latitude]
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [place.longitude, place.latitude]
                 },
-                "properties": {
-                    "title": place.title,
-                    "placeId": place.id,
-                    "detailsUrl": reverse(place_details, kwargs={"place_id": place.id})
+                'properties': {
+                    'title': place.title,
+                    'placeId': place.id,
+                    'detailsUrl': reverse(place_details, kwargs={'place_id': place.id})
                 }
             }
         )
-    return render(request, "index.html", context={"places_geojson": places_geojson})
+    return render(request, 'index.html', context={'places_geojson': places_geojson})
 
 
 def place_details(request, place_id):
     place = get_object_or_404(Place, id=place_id)
     data = {
-        "title": place.title,
-        "imgs": [image.file.url for image in place.images.all()],
-        "description_short": place.description_short,
-        "description_long": place.description_long,
-        "coordinates": {
-            "lng": place.longitude,
-            "lat": place.latitude
+        'title': place.title,
+        'imgs': [image.file.url for image in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.longitude,
+            'lat': place.latitude
         }
     }
     return JsonResponse(data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
